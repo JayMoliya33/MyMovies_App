16004d12e92b3306715f47c4de3e43de0b77cfc5
Jay Moliya <52040275+JayMoliya33@users.noreply.github.com>
2021-02-18 14:18:48 +0530

    updated

--- app/src/main/java/com/example/moviesapp/api/MoviesAPI.kt
+++ app/src/main/java/com/example/moviesapp/api/MoviesAPI.kt
@@ -4,6 +4,7 @@ import com.example.moviesapp.model.MoviesResponse
 import com.example.moviesapp.util.Constants.Companion.API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesAPI {
@@ -31,4 +32,10 @@ interface MoviesAPI {
             @Query("api_key")
             apiKey: String = API_KEY
     ): Response<MoviesResponse>
+
+    @GET("movie/{movie_id}")
+    fun getMovieDetail(@Path("movie_id") movieId: Int): Response<MoviesResponse>
+
+    @GET("search/movie")
+    fun searchMovie(@Query("query") query: String, @Query("page") page: Int): Response<MoviesResponse>
 }
